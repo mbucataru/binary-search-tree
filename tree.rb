d4646c03788950d9c95f3089369cb305d61e7864
mbucataru <mbucatarugithub@gmail.com>
2023-08-23 19:28:43 +0300

    Add rough framework for #build_tree

--- tree.rb
+++ tree.rb
@@ -1,4 +1,18 @@
 require_relative 'node'
 class Tree
+  def initialize(array)
+    @root = build_tree(array)
+  end
 
+  def build_tree(array)
+    array.uniq!.sort!
+    root = array[array.length / 2]
+
+    # Build right side of tree
+    build_tree(array[0...root])
+    # Build left side of tree
+    build_tree(array[root + 1...array.length])
+
+    root
+  end
 end
