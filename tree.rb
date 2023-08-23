c874430910a9a147a2ae9bc5a729a0a906ec2e71
mbucataru <mbucatarugithub@gmail.com>
2023-08-23 19:59:29 +0300

    Complete #build_tree and add #pretty_print

--- tree.rb
+++ tree.rb
@@ -5,14 +5,26 @@ class Tree
   end
 
   def build_tree(array)
-    array.uniq!.sort!
-    root = array[array.length / 2]
+    return Node.new(array[0]) if array.length <= 1
 
-    # Build right side of tree
-    build_tree(array[0...root])
+    array.uniq!
+    array.sort!
+    midpoint = array.length / 2
+    root = Node.new(array[midpoint])
     # Build left side of tree
-    build_tree(array[root + 1...array.length])
-
+    root.left_child = build_tree(array[0...midpoint])
+    # Build right side of tree
+    root.right_child = build_tree(array[midpoint + 1...array.length])
     root
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end
 end
+
+tree = Tree.new([1, 2, 3, 4])
+# [0, 1, 3, 4, 5, 9]
+tree.pretty_print
